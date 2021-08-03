cee7be2b144cc65ee76d8bd8eb9c498a6eb69205
kirinnee <ernest@eng.atomi.cloud>
2021-08-03 14:47:21 +0800

    update(sg): from 0.1.5 to 0.1.6

--- node/node-packages.nix
+++ node/node-packages.nix
@@ -6766,10 +6766,10 @@ in
   "@atomi-user-first/semantic-generator" = nodeEnv.buildNodePackage {
     name = "_at_atomi-user-first_slash_semantic-generator";
     packageName = "@atomi-user-first/semantic-generator";
-    version = "0.1.5";
+    version = "0.1.6";
     src = fetchurl {
-      url = "https://registry.npmjs.org/@atomi-user-first/semantic-generator/-/semantic-generator-0.1.5.tgz";
-      sha512 = "hyv0cDL7OUtikl5QldAcfa65VgKa8kC4HN33icXuhLmsoOHclqkgy6LGr4ty/ruEyfoLlOcnsoBuEukzQLtMUA==";
+      url = "https://registry.npmjs.org/@atomi-user-first/semantic-generator/-/semantic-generator-0.1.6.tgz";
+      sha512 = "ksWkjEOO3Lq/ZlbIn+UusqkMW2S3tnAgj62L9eckhLm42L/Vq2dTZni7hASrLD1v5VqAgQS3NouaiClV/OyIoA==";
     };
     dependencies = [
       sources."@babel/code-frame-7.14.5"

d78e58dd8bed346631151188a455cda9bad9a864
kirinnee <ernest@eng.atomi.cloud>
2021-08-03 19:44:21 +0800

    ci: update CI

--- env.nix
+++ env.nix
@@ -1,6 +1,51 @@
 let nixpkgs = import <nixpkgs> { }; in
 let pkgs = import ./packages.nix { inherit nixpkgs; }; in
-nixpkgs.buildEnv {
-  name = "dev-env";
-  paths = pkgs;
+with pkgs;
+{
+  dev = nixpkgs.buildEnv {
+    name = "dev-env";
+    paths = [
+      gitlint
+      pls
+      please
+      plz
+      pre-commit
+      git
+      shfmt
+      shellcheck
+      nixpkgs-fmt
+      nix-prefetch
+      sd
+      coreutils
+      prettier
+      bash
+    ];
+  };
+  ci = nixpkgs.buildEnv {
+    name = "ci-env";
+    paths = [
+      gitlint
+      pls
+      please
+      plz
+      pre-commit
+      git
+      shfmt
+      shellcheck
+      nixpkgs-fmt
+      nix-prefetch
+      sd
+      coreutils
+      prettier
+      bash
+    ];
+  };
+
+  cd = nixpkgs.buildEnv {
+    name = "cd-env";
+    paths = [
+      sg
+      pnpm
+    ];
+  };
 }

94c5a62ce2de303a9f5ca0fadf97a79aef6d8b65
kirinnee <kirinnee97@gmail.com>
2022-10-05 14:02:44 +0800

    config(env): updated nix

--- nuget/common.nix
+++ nuget/common.nix
@@ -1,29 +1,35 @@
 { nixpkgs, runtime, name, version, sha256, meta }:
 with nixpkgs;
-let nuget = fetchNuGet {
-  pname = name;
-  baseName = name;
-  version = version;
-  sha256 = sha256;
-  outputFiles = [ "*" ];
-  postUnpack = ''
-    chmod -R +r .
+let
+  nuget = fetchNuGet {
+    pname = name;
+    baseName = name;
+    version = version;
+    sha256 = sha256;
+    outputFiles = [ "*" ];
+    postUnpack = ''
+      chmod -R +r .
+    '';
+  };
+in
+let
+  dlls = stdenv.mkDerivation {
+    pname = name;
+    src = nuget;
+    installPhase = ''
+      mkdir -p $out/
+      dll="$(find $src -name '${name}.dll')"
+      echo "$dll" > $out/path
+    '';
+    inherit name meta version;
+  };
+in
+let
+  bin = writeShellScriptBin name ''
+    path_to_dll="$(${coreutils}/bin/cat ${dlls}/path)"
+    ${runtime}/bin/dotnet "$path_to_dll" $@
   '';
-}; in
-let dlls = stdenv.mkDerivation {
-  pname = name;
-  src = nuget;
-  installPhase = ''
-    mkdir -p $out/
-    dll="$(find $src -name '${name}.dll')"
-    echo "$dll" > $out/path
-  '';
-  inherit name meta version;
-}; in
-let bin = writeShellScriptBin name ''
-  path_to_dll="$(${coreutils}/bin/cat ${dlls}/path)"
-  ${runtime}/bin/dotnet "$path_to_dll" $@
-''; in
+in
 stdenv.mkDerivation ({
   inherit version meta;
   name = "${name}-${version}";

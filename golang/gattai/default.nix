a05198d8c7c4fa4c5386b68753719a6980274da1
tanyaowei <yaowei@tr8.io>
2022-10-12 17:16:25 +0800

    update(gattai): from v0.1.0 to v0.1.1

--- golang/gattai/default.nix
+++ golang/gattai/default.nix
@@ -2,18 +2,18 @@
 with nixpkgs;
 buildGoModule rec {
   pname = "gattai";
-  version = "v0.1.0";
+  version = "v0.1.1";
 
   meta = {
     owner = "tr8team";
     repo = "gattai";
     rev = version;
-    sha256 = "02ph8c97cxcs97ppz1r2j94bjjmjx6akzwdqb1176hhfma0f5c18";
+    sha256 = "03za0mr0hk7yyksinjpxrnvn90dihdwvijzx33rzpa786jcmjrn9";
   };
 
   src = fetchFromGitHub meta;
 
-  vendorSha256 = "sha256-il2i6hy0ijfwN1+2DJC75yOaevuMzatnGgF1X/7V4Uo=";
+  vendorSha256 = "sha256-MYgCOOQJ7Ge0LbTxOkQWhxzqTxAgrxYmBs+WIkFxBz4=";
 
   doCheck = false;
 

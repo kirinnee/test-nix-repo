90ee0978eec8516c9487c398827c4f849f02edde
kirinnee <kirinnee97@gmail.com>
2023-07-12 23:47:03 +0800

    config: atomi classic shim for critical package sg

--- default.nix
+++ default.nix
@@ -1,42 +1,16 @@
 let
-  nixpkgs = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/554d2d8aa25b6e583575459c297ec23750adb6cb.tar.gz") { };
+  nixpkgs = import (fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/554d2d8aa25b6e583575459c297ec23750adb6cb.tar.gz";
+    sha256 = "sha256:01yfqslnkyrfb5yjfablhvw830iw0za3mab4n03a0ldyq5ac6wh1";
+  }) { };
 
   # Node
   npm = (
     let n = import ./node/export.nix { inherit nixpkgs; }; in
     with n; {
-      inherit cyanprint semantic-release-cli markdown-table-prettify sg vercel;
+      inherit sg;
     }
   );
-
-  # Shell
-  shell = (
-    with (import ./shellWrapper/pls/default.nix { inherit nixpkgs; });
-    {
-      inherit pls please plz;
-    }
-  );
-
-  # Python
-  python = {
-    # gitlint = import ./python/gitlint/default.nix { inherit nixpkgs; };
-  };
-
-  # Go
-  golang = {
-    narwhal = import ./golang/narwhal/default.nix { inherit nixpkgs; };
-    narwhal_0_3_11 = import ./golang/narwhal/0.3.11.nix { inherit nixpkgs; };
-  };
-
-  # JetBrains IDE
-  jetbrains = import ./jetbrains/default.nix { inherit nixpkgs; };
-
-  # dotnet
-  # dotnet = (with import ./nuget/dotnet.nix { inherit nixpkgs; }; {
-  #   inherit dotnetsay;
-  # });
-  dotnet = { };
 in
-
 # merge
-npm // shell // python // golang // dotnet // jetbrains
+npm
--- node/export.nix
+++ node/export.nix
@@ -2,7 +2,10 @@
 let
   n = import ./composition.nix { pkgs = nixpkgs; };
   npkgs = {
-    "25th July 2021 21.05" = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/537678cb1ead06fca831077c3b193566cbc3f406.tar.gz") { };
+    "25th July 2021 21.05" = import (fetchTarball {
+      url = "https://github.com/NixOS/nixpkgs/archive/537678cb1ead06fca831077c3b193566cbc3f406.tar.gz";
+      sha256 = "sha256:01yfqslnkyrfb5yjfablhvw830iw0za3mab4n03a0ldyq5ac6wh1";
+    }) { };
   };
 in
 with n;

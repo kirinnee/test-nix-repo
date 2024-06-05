a559e02891f99227be167babc2fafc2fc259e555
kirinnee <kirinnee97@gmail.com>
2024-06-05 15:47:49 +0800

    ci: temporarily fix releaser

--- scripts/ci/release.sh
+++ scripts/ci/release.sh
@@ -1,3 +1,4 @@
 #! /bin/sh
 rm .git/hooks/*
+npm i -D conventional-changelog-conventionalcommits@7.0.2 @semantic-release/release-notes-generator@12.0.0 @semantic-release/commit-analyzer@12.0.0
 sg release -i npm || true
